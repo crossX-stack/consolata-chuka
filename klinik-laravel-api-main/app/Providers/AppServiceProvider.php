90cad7136bc0672a1aa6ff908beebe236c3e5a28
crossX-stack <victorkarani35@gmail.com>
2025-04-01 05:46:58 +0300

    Update AppServiceProvider.php

--- klinik-laravel-api-main/app/Providers/AppServiceProvider.php
+++ klinik-laravel-api-main/app/Providers/AppServiceProvider.php
@@ -1,11 +1,9 @@
-<?php
+ <?php
 
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,13 +23,5 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
-
-        // **Auto-login as admin**
-        if (app()->runningInConsole() === false) { // Prevent errors in CLI commands
-            $admin = User::where('is_admin', true)->first();
-            if ($admin) {
-                Auth::login($admin);
-            }
-        }
     }
 }
